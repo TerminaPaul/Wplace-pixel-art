4b8268ed474862742d78dfaa211b594c393a627f
Paul <paul.ull.leathers@gmail.com>
2026-03-26 17:35:35 +0100

    a

--- Redimensionner.py
+++ Redimensionner.py
@@ -1,34 +1,37 @@
 from PIL import Image
+from collections import deque
+
 
 class Redimensionner(object):
     def __init__(self, name):
-        self.img = Image.open(name + ".png").convert("RGBA")
+        self.img = Image.open(name + ".png").convert("RGBA").copy()
         self.width = self.img.size[0]
         self.height = self.img.size[1]
         self.fond = self.img.getpixel((0, 0))
 
     def transparent(self, t=50):
-        blank = [(0, 0)]
+        blank = deque()
+        blank.append((0, 0))
         visited = set()
         visited.add((0, 0))
 
         while blank:
-            x, y = blank[0]
-
+            x, y = blank.popleft()
             if self.condition_pixel_est_invisible(x, y, t):
                 self.img.putpixel((x, y), (50, 50, 50, 0))
                 for nx, ny in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
                     if (nx, ny) not in visited and 0 <= nx < self.width and 0 <= ny < self.height:
                         visited.add((nx, ny))
                         blank.append((nx, ny))
-
-            blank.remove(blank[0])
-
+            else:
+                visited.add((x, y))
         self.img.show()
 
     def condition_pixel_est_invisible(self, x, y, t):
         R, G, B, A = self.fond
         r, g, b, a = self.img.getpixel((x, y))
-        if R - t <= r <= R + t and G - t <= g <= G + t and B - t <= b <= B + t or a == 0:
+        if (R - t <= r <= R + t and G - t <= g <= G + t and B - t <= b <= B + t) or a == 0:
+            print("True")
             return True
+        print("False")
         return False
