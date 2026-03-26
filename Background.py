cf6e5c41e12a87dd7e4fb388b96857eff889755b
Paul <paul.ull.leathers@gmail.com>
2026-03-26 18:30:46 +0100

    ouais c'est greg

--- Background.py
+++ Background.py
@@ -2,21 +2,22 @@ from PIL import Image
 from collections import deque
 
 
-class Redimensionner(object):
+class Background(object):
     def __init__(self, name):
         self.img = Image.open(name + ".png").convert("RGBA").copy()
         self.width = self.img.size[0]
         self.height = self.img.size[1]
         self.fond = self.img.getpixel((0, 0))
 
-    def transparent(self, t=50):
+    def transparent(self, start_x=0, start_y=0, t=50):
         blank = deque()
-        blank.append((0, 0))
+        blank.append((start_x, start_y))
         visited = set()
-        visited.add((0, 0))
+        visited.add((start_x, start_y))
 
         while blank:
             x, y = blank.popleft()
+
             if self.condition_pixel_est_invisible(x, y, t):
                 self.img.putpixel((x, y), (50, 50, 50, 0))
                 for nx, ny in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
@@ -25,13 +26,15 @@ class Redimensionner(object):
                         blank.append((nx, ny))
             else:
                 visited.add((x, y))
+
         self.img.show()
 
     def condition_pixel_est_invisible(self, x, y, t):
         R, G, B, A = self.fond
         r, g, b, a = self.img.getpixel((x, y))
         if (R - t <= r <= R + t and G - t <= g <= G + t and B - t <= b <= B + t) or a == 0:
-            print("True")
             return True
-        print("False")
         return False
+
+    def save(self, name):
+        self.img.save(name + ".png")
--- Main.py
+++ Main.py
@@ -1,8 +1,29 @@
-from Redimensionner import Redimensionner
+from Background import Background
 
 if __name__ == '__main__':
-    #img = (Redimensionner("test1"))
-    #img.background_transparent()
-    img = (Redimensionner("test1"))
-    img.condition_pixel_est_invisible(0,0,50)
-    img.transparent()
+    x = int(input('Donner la coordonnée du point "x" : '))
+    y = int(input('Donner la coordonnée du point "y" : '))
+    img = (Background("test1"))
+    img.transparent(x, y)
+
+    while True:
+        answer = input("Continuer les modifications ? \noui/non/retour")
+
+        if answer == "oui":
+            img.save("test1_result")
+            x = int(input('Donner la coordonnée du point "x" : '))
+            y = int(input('Donner la coordonnée du point "y" : '))
+            img = Background("test1_result")
+            img.transparent(x, y)
+
+        elif answer == "non":
+            img.save("test1_result")
+            print("Ok image sauvegardée")
+            break
+
+        elif answer == "retour":
+            print("Retour a l'étape precedent")
+            x = int(input('Donner la coordonnée du point "x" : '))
+            y = int(input('Donner la coordonnée du point "y" : '))
+            img = Background("test1_result")
+            img.transparent(x, y)
